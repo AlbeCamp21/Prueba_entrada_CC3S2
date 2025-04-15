d5d669d2ad06b864cf0b058ded5c3d0f5e2ef174
AlbeCamp21 <luis.alanya.c@uni.pe>
2025-04-14 22:38:04 -0500

    main.py actualizado para implementar routes

--- trivia-game-python/app/main.py
+++ trivia-game-python/app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from app.routes.questions import router as questions_router
 
 app = FastAPI()
 
+app.include_router(questions_router, prefix="/questions")
+
 @app.get("/")
 def read_root():
-    return {"message": "hola"}
+	return {"message": "hola"}
+
