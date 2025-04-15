a6f00e43b9a44d6fdb2c7bb0c4fbf47482ddc739
AlbeCamp21 <luis.alanya.c@uni.pe>
2025-04-15 18:20:51 -0500

    config.py actualizado

--- trivia-game-python/app/config.py
+++ trivia-game-python/app/config.py
@@ -1,12 +1,8 @@
 from dotenv import load_dotenv, find_dotenv
 import os
 
-# Carga automáticamente el archivo .env más cercano
 dotenv_path = find_dotenv()
 load_dotenv(dotenv_path)
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 SECRET_KEY = os.getenv("SECRET_KEY")
-
-print(f"[DEBUG] dotenv_path: {dotenv_path}")
-print(f"[DEBUG] DATABASE_URL: {DATABASE_URL}")
