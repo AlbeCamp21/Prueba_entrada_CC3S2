58508d0c1f6916fffd4858e3806c38e90ada5960
AlbeCamp21 <luis.alanya.c@uni.pe>
2025-04-15 19:21:45 -0500

    Test actualizado para prueba

--- trivia-game-python/app/test_trivia.py
+++ trivia-game-python/app/test_trivia.py
@@ -12,7 +12,7 @@ def test_question_incorrect_answer():
 def test_quiz_scoring():
 	quiz=Quiz()
 	question = Question("What is 2 + 2?", ["1", "2", "3", "4"], "4")
-	quiz.add_question(question)
+	await quiz.add_question(question)
 	assert quiz.answer_question(question, "4") == True
-	assert quiz.correct_answers == 1
+	assert quiz.correct_answers == 4
 
